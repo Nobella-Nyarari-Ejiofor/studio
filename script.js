1f858682d1c72d15e1b56b6bfc1e0ec4186d947a
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2021-11-22 13:59:12 -0500

    Added styling to the what we do section using jquery

---
+++ script.js
@@ -0,0 +1,41 @@
+function whatToDo2(){
+  $('.p2').hide();
+      if($('.img-2').on('click',function(){
+          $('.p2').show();
+          $('.img-2').hide();
+      })){}
+      if($('.p2').on('click',function(){
+          $('.img-2').show();
+          $('.p2').hide();
+      })){}
+}
+whatToDo2();
+
+function whatToDo1(){
+  // Hiding the paragraph
+  $('.p1').hide();
+   if($('.img-1').on('click',function(){
+      $('.img-1').hide();
+      $('.p1').show();
+      
+   })){};
+   if($('.p1').on('click',function(){
+     $('.p1').hide();
+     $('.img-1').show();
+     
+   })){};
+};
+whatToDo1()
+function whatToDo3(){
+  // Hiding the paragraph
+  $('.p3').hide();
+   if($('.img-3').on('click',function(){
+      $('.p3').show();
+      $('.img-3').hide();
+   })){};
+   if($('.p3').on('click',function(){
+     $('.img-3').show();
+     $('.p3').hide();
+   })){};
+};
+whatToDo3()
