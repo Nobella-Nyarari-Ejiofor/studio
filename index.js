c9d4e3039669a6ae5b6526082129c899d69fc684
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2021-11-22 14:58:53 -0500

    Added a javascript code for the alert

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+forms.addEventListener('submit',message);
 function message(){
-   let nameAns = document.getElementById('name')
-   let emailAns = document.getElementById('email')
-   alert(nameAns.value)
+   let nameAns = document.getElementById('name') ;
+   let emailAns = document.getElementById('email') ;
+   let messageAns = document.getElementById("message")
+   if(nameAns.value =="" && emailAns.value =="" ){
+     alert("Error please input both your name and email") ;
+   } else if (nameAns.value == " " && emailsAns == " " && messageAns.value == " " ){
+    alert(nameAns.value + "  we have received your message. Thank you for reaching out to us. ")
+   }else {
+    alert("Thank you for your providing us with your name and email"+ " " + nameAns.value)
+   }
+
 };
-message()
