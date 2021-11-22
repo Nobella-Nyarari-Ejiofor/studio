6f1a4fbd812d7bd8ebdcf7e70bcd104cab3d3361
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2021-11-22 15:21:57 -0500

    Added the all alerts needed

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function message(){
    let messageAns = document.getElementById("message")
    if(nameAns.value =="" && emailAns.value =="" ){
      alert("Error please input both your name and email") ;
-   } else if (nameAns.value == " " && emailsAns == " " && messageAns.value == " " ){
+   } else if ((nameAns.value !== "" && emailAns !== "" )&& messageAns.value !== "" ){
     alert(nameAns.value + "  we have received your message. Thank you for reaching out to us. ")
    }else {
     alert("Thank you for your providing us with your name and email"+ " " + nameAns.value)
    }
-
+   event.preventDefault()
 };
